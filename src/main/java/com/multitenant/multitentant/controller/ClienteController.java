1bbfe9f6e1caf29bbe34862d155257e1a4972ddb
Andres9312 <andres00fonseca@gmail.com>
2021-09-29 19:32:57 -0500

    Paquetes model, dao, service y controller creados

--- src/main/java/com/multitenant/multitentant/controller/ClienteController.java
+++ src/main/java/com/multitenant/multitentant/controller/ClienteController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/sucursal")
+@RequestMapping("/api/cliente")
 @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 
 public class ClienteController {
